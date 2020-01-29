4b9b1772968ccd5e2f41786f78a59e8e798c076e
tisanai <tisanai@10.4.32.94>
2020-01-29 12:20:07 +0700

    Test2 - del Test - edit

--- src/view/Test.java
+++ src/view/Test.java
@@ -16,5 +16,6 @@ public class Test {
     public static void main(String[] args) {
         Student stu = new Student(1, "John");
         System.out.println(stu);
+        System.out.println(stu);
     }
 }
